ed6473abaf88762135262c3770a590083dc9a2a7
robotic-manipulators <robotic_manipulators@outlook.com>
2026-03-17 16:49:54 +0800

    Delete src/__init__.py

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/__init__.py
-from .camera import StaticTripodCamera
-from .kalman import KalmanFilter3D
-from .robot_utils import get_robot_full_state, solve_optimization_full_body
-from .simulation import setup_simulation, run_experiment
-from . import config
